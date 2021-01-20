441439ef4ca2c624df1c14a868068a31e1fbbb51
Daoud <99m.daoud@gmail.com>
2021-01-20 16:16:00 +0100

    aggiunti model e controller

--- database/migrations/2021_01_20_144042_create_products_table.php
+++ database/migrations/2021_01_20_144042_create_products_table.php
@@ -15,7 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 30);
+            $table->string('name', 30)->nullable();
             $table->string('type', 30);
             $table->decimal('price', 8, 2);
             $table->timestamps();
